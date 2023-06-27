2cc25392bd47fdb20f11c41a03016f64a67c9b61
Sarah Bruce <43051562+sjbruce3301@users.noreply.github.com>
2023-06-27 10:09:50 -0400

    save logplot

--- Code/ACES_newcam_tester.m
+++ Code/ACES_newcam_tester.m
@@ -25,8 +25,33 @@ title("10,000hz Framegrab (moving) Set 1")
 xlabel("Frame")
 ylabel("Average Intensity")
 
-x = randi([0, 255], 600, 600, 28, 'uint8');
-v = VideoWriter('myvideo_grayscale.avi', 'Grayscale AVI');
-open(v);
-writeVideo(v, x);
-close(v)
+fileID = fopen('mimic_log_nofilter_moving_1.bin');
+
+log = fopen('mimic_log_nofilter_moving_1.bin');
+log_dat = fread(log);
+%plot(log_dat)
+
+%fclose(log);
+% Prepare the new file.
+vidObj = VideoWriter('trial2_set3.avi');
+open(vidObj);
+
+frameset = data(10,312,:);
+
+ims_set = cell(length(frameset), 1);
+
+for u = 1:length(frameset)
+    im1 = (data(:,:,frames));
+    imwrite(double(im1), "image.png")
+    ims_set{u} = imread('image.png');
+end
+
+% Create an animation.
+% write the frames to the video
+for v=1:length(frameset)
+    % convert the image to a frame
+    frame = im2frame(ims_set{v});
+    writeVideo(vidObj, frame);
+end
+% Close the file.
+close(vidObj);
