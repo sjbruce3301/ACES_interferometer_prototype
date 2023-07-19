cdc10dd97adbb62639e2782ff1f45d49e5489de4
Sarah Bruce <43051562+sjbruce3301@users.noreply.github.com>
2023-07-19 10:59:08 -0400

    i_constant interpolation

--- Code/laser_analysis.m
+++ Code/laser_analysis.m
@@ -157,6 +157,9 @@ fourier_transform = fft(intensity_plot(1726:1760));
 
 %%
 timealigned_camera = ts_sec + 0.75;
-camera_pos = interp1(t1, x1, timealigned_camera);
+camera_pos_x = interp1(t1, x1, timealigned_camera);
 
-
+x_cam_window = camera_pos_x(6300:6750);
+i_window = intensity_plot(6300:6750);
+x_const = 0:.005:4;
+i_const = interp1(unique(x_cam_window), i_window(1:435), unique(x_const));
